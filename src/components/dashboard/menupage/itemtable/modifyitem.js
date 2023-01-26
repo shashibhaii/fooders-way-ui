09f7ad6c7339c9d30bcddc27eaabb7823abdb126
Utsav <utsavpokemon9000chatterjee@gmail.com>
2023-01-26 21:14:44 +0530

    fix modify modal

--- src/components/dashboard/menupage/itemtable/modifyitem.js
+++ src/components/dashboard/menupage/itemtable/modifyitem.js
@@ -94,7 +94,8 @@ function Modify({ close, setToastDanger, setToastSuccess, setAllertmsg, row, set
             <hr />
             <div className="px-4 py-5">
               <label className="relative inline-flex items-center mb-4 cursor-pointer">
-                {isEnabled === 'YES' ? <input type="checkbox" onClick={() => setIsEnabled('NO')} className="sr-only peer" defaultChecked /> : <input type="checkbox" onClick={() => setIsEnabled('YES')} className="sr-only peer" />}
+                {row&&row.isEnabled === 'YES' && <input type="checkbox" onClick={() => setIsEnabled('NO')} className="sr-only peer" defaultChecked />} 
+                {row&&row.isEnabled === 'NO' &&<input type="checkbox" onClick={() => setIsEnabled('YES')} className="sr-only peer" />}
                 <div className="w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-blue-300 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-blue-600">
                 </div>
                 <span className="ml-3 text-sm font-medium text-gray-900">Item Available</span>
