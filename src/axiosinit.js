9a0dc7140bfa1937ec00bb5bcda6bf80c27070b0
Shashikant Shaw <shashikant.shaw@buyogo.com>
2025-01-25 23:53:20 +0530

    temp

--- src/axiosinit.js
+++ src/axiosinit.js
@@ -5,22 +5,23 @@ import axios from 'axios';
 
 
 export const unauthorizedaxios = axios.create({
-    baseURL: 'https://food-service.osc-fr1.scalingo.io/api',
+    baseURL: 'http://food-service-env-1.eba-es8trfam.us-east-1.elasticbeanstalk.com/api',
     headers: {
         'Access-Control-Allow-Origin': '*',
         'Content-Type': 'application/json',
-
+        "ngrok-skip-browser-warning":"any",
     },
 
 });
 
 export const authorizedaxios = (token, businessId) => axios.create({
-    baseURL: 'https://food-service.osc-fr1.scalingo.io/api',
+    baseURL: 'http://food-service-env-1.eba-es8trfam.us-east-1.elasticbeanstalk.com/api',
     headers: {
         Authorization: `Bearer ${token}`,
         'Access-Control-Allow-Origin': '*',
         'Content-Type': 'application/json',
-        'x-business-id': businessId
+        'x-business-id': businessId,
+        "ngrok-skip-browser-warning":"any",
     }
 
 })
